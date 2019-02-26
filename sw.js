7544a705733ac0194971d634c97ac629806cdce5
sneddybob <bobsned12@gmail.com>
2019-02-26 12:30:44 -0600

    sw update

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-self.addEventListener('install', function(event) {
-    var CACHE_NAME = 'my-site-cache-v1';
+
+
+var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/script/main.js'
+  '',
+  'main.css',
+  'app.js'
 ];
 
 self.addEventListener('install', function(event) {
@@ -16,4 +17,17 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
   });
